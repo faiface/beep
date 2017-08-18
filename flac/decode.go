f94f847915b0e7beb4276eed36adc90d8716368e
mewmew <rnd0x00@gmail.com>
2017-08-18 20:35:26 +0200

    flac: add support for 8 and 24 bit rate

--- flac/decode.go
+++ flac/decode.go
@@ -87,10 +87,20 @@ func (d *decoder) refill() error {
 	bps := d.stream.Info.BitsPerSample
 	nchannels := d.stream.Info.NChannels
 	switch {
+	case bps == 8 && nchannels >= 2:
+		for i := 0; i < n; i++ {
+			d.buf[i][0] = float64(int16(frame.Subframes[0].Samples[i])) / (1<<7 - 1)
+			d.buf[i][1] = float64(int16(frame.Subframes[1].Samples[i])) / (1<<7 - 1)
+		}
 	case bps == 16 && nchannels >= 2:
 		for i := 0; i < n; i++ {
 			d.buf[i][0] = float64(int16(frame.Subframes[0].Samples[i])) / (1<<15 - 1)
 			d.buf[i][1] = float64(int16(frame.Subframes[1].Samples[i])) / (1<<15 - 1)
+		}
+	case bps == 24 && nchannels >= 2:
+		for i := 0; i < n; i++ {
+			d.buf[i][0] = float64(int16(frame.Subframes[0].Samples[i])) / (1<<23 - 1)
+			d.buf[i][1] = float64(int16(frame.Subframes[1].Samples[i])) / (1<<23 - 1)
 		}
 	default:
 		panic(fmt.Errorf("support for %d bits-per-sample and %d channels combination not yet implemented", bps, nchannels))
