2bbbbb416520613ef8b5f52d58f06c522b14191a
faiface <faiface2202@gmail.com>
2019-03-04 14:41:58 +0100

    effects: add doc for Doppler

--- effects/doppler.go
+++ effects/doppler.go
@@ -2,6 +2,22 @@ package effects
 
 import "github.com/faiface/beep"
 
+// Doppler simulates a "sound at a distance". If the sound starts at a far distance,
+// it'll take some time to reach the ears of the listener.
+//
+// The distance of the sound can change dynamically. Doppler adjusts the density of
+// the sound (affecting its speed) to remain consistent with the distance. This is called
+// the Doppler effect.
+//
+// The arguments are:
+//
+//   quality:         the quality of the underlying resampler (1 or 2 is usually okay)
+//   samplesPerMeter: sample rate / speed of sound
+//   s:               the source streamer
+//   distance:        a function to calculate the current distance; takes number of
+//                    samples Doppler wants to stream at the moment
+//
+// This API is experimental and may change any time!
 func Doppler(quality int, samplesPerMeter float64, s beep.Streamer, distance func(delta int) float64) beep.Streamer {
 	return &doppler{
 		r:               beep.ResampleRatio(quality, 1, s),
