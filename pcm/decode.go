c28020dcbb6cfa516ba247a7f943d1b836c8a309
Ilia Choly <ilia.choly@gmail.com>
2020-11-11 16:29:39 -0500

    pcm: add package for raw PCM decoding

---
+++ pcm/decode.go
@@ -0,0 +1,60 @@
+package pcm
+
+import (
+	"io"
+
+	"github.com/faiface/beep"
+)
+
+type stream struct {
+	r   io.Reader
+	f   beep.Format
+	buf []byte
+	len int
+	pos int
+	err error
+}
+
+func Decode(r io.Reader, format beep.Format) beep.Streamer {
+	return &stream{
+		r:   r,
+		f:   format,
+		buf: make([]byte, 512*format.Width()),
+	}
+}
+
+func (s *stream) Err() error { return s.err }
+
+func (s *stream) Stream(samples [][2]float64) (n int, ok bool) {
+	width := s.f.Width()
+	// decode any left-over data
+	for n < len(samples) && s.len-s.pos >= width {
+		samples[n], _ = s.f.DecodeSigned(s.buf[s.pos:])
+		n++
+		s.pos += width
+	}
+	// if the samples are full, we're done
+	if n == len(samples) {
+		return n, true
+	}
+	// if there's a partial frame, move it to the beginning of the buffer
+	if s.len-s.pos != 0 {
+		copy(s.buf, s.buf[s.pos:s.len])
+	}
+	s.len = s.len - s.pos
+	s.pos = 0
+	// refill the buffer
+	nbytes, err := s.r.Read(s.buf[s.len:])
+	if err != nil {
+		s.err = err
+		return n, false
+	}
+	s.len += nbytes
+	// decode as many samples as we can
+	for n < len(samples) && s.len-s.pos >= width {
+		samples[n], _ = s.f.DecodeSigned(s.buf[s.pos:])
+		n++
+		s.pos += width
+	}
+	return n, true
+}
---
+++ pcm/encode.go
@@ -0,0 +1,29 @@
+package pcm
+
+import (
+	"bufio"
+	"io"
+
+	"github.com/faiface/beep"
+)
+
+func Encode(w io.Writer, s beep.Streamer, format beep.Format) error {
+	var (
+		bw      = bufio.NewWriter(w)
+		samples = make([][2]float64, 512)
+		buffer  = make([]byte, len(samples)*format.Width())
+	)
+	for {
+		n, ok := s.Stream(samples)
+		if !ok {
+			return bw.Flush()
+		}
+		var offset int
+		for _, sample := range samples[:n] {
+			offset += format.EncodeSigned(buffer[offset:], sample)
+		}
+		if _, err := bw.Write(buffer[:offset]); err != nil {
+			return err
+		}
+	}
+}
