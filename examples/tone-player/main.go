451b3a82a45762b2b9ea39a8b367ed54de4520fb
ilyapashuk <ilusha.paschuk@gmail.com>
2021-08-31 17:35:42 +0300

    code linting

--- examples/tone-player/main.go
+++ examples/tone-player/main.go
@@ -1,20 +1,21 @@
 package main
 
-import "os"
-import "github.com/faiface/beep/generators"
-import "github.com/faiface/beep/speaker"
-import "github.com/faiface/beep"
-import "strconv"
-
+import (
+	"os"
+	"github.com/faiface/beep/generators"
+	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep"
+	"strconv"
+)
 func main() {
-f,_ := strconv.Atoi(os.Args[1])
-speaker.Init(beep.SampleRate(48000), 4800)
-s,err := generators.SinTone(beep.SampleRate(48000), f)
-if err != nil {
-panic(err)
-}
-speaker.Play(s)
-for {
+	f, _ := strconv.Atoi(os.Args[1])
+	speaker.Init(beep.SampleRate(48000), 4800)
+	s, err := generators.SinTone(beep.SampleRate(48000), f)
+	if err != nil {
+		panic(err)
+	}
+	speaker.Play(s)
+	for {
 
-}
+	}
 }
