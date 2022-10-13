b91efebe54ada048ad6064ae9703b2981b8b7218
Mark Kremer <mail@markkremer.nl>
2022-10-13 17:47:09 +0200

    Upgrade to Oto v2 & let Oto pull samples instead of pushing them to Oto

--- speaker/speaker.go
+++ speaker/speaker.go
@@ -2,21 +2,22 @@
 package speaker
 
 import (
-	"sync"
-
 	"github.com/faiface/beep"
-	"github.com/hajimehoshi/oto"
+	"github.com/hajimehoshi/oto/v2"
 	"github.com/pkg/errors"
+	"io"
+	"sync"
 )
+
+const channelCount = 2
+const bitDepthInBytes = 2
+const bytesPerSample = bitDepthInBytes * channelCount
 
 var (
 	mu      sync.Mutex
 	mixer   beep.Mixer
-	samples [][2]float64
-	buf     []byte
 	context *oto.Context
-	player  *oto.Player
-	done    chan struct{}
+	player  oto.Player
 )
 
 // Init initializes audio playback through speaker. Must be called before using this package.
@@ -25,36 +26,23 @@ var (
 // bufferSize means lower CPU usage and more reliable playback. Lower bufferSize means better
 // responsiveness and less delay.
 func Init(sampleRate beep.SampleRate, bufferSize int) error {
-	mu.Lock()
-	defer mu.Unlock()
-
-	Close()
+	if context != nil {
+		return errors.New("speaker cannot be initialized more than once")
+	}
 
 	mixer = beep.Mixer{}
 
-	numBytes := bufferSize * 4
-	samples = make([][2]float64, bufferSize)
-	buf = make([]byte, numBytes)
-
 	var err error
-	context, err = oto.NewContext(int(sampleRate), 2, 2, numBytes)
+	var readyChan chan struct{}
+	context, readyChan, err = oto.NewContext(int(sampleRate), channelCount, bitDepthInBytes)
 	if err != nil {
 		return errors.Wrap(err, "failed to initialize speaker")
 	}
-	player = context.NewPlayer()
+	<-readyChan
 
-	done = make(chan struct{})
-
-	go func() {
-		for {
-			select {
-			default:
-				update()
-			case <-done:
-				return
-			}
-		}
-	}()
+	player = context.NewPlayer(newReaderFromStreamer(&mixer))
+	player.(oto.BufferSizeSetter).SetBufferSize(bufferSize * bytesPerSample)
+	player.Play()
 
 	return nil
 }
@@ -63,19 +51,16 @@ func Init(sampleRate beep.SampleRate, bufferSize int) error {
 // even when the program doesn't play anymore, because in properly set systems, the default mixer
 // handles multiple concurrent processes. It's only when the default device is not a virtual but hardware
 // device, that you'll probably want to manually manage the device from your application.
+//
+// TODO: investigate what happens now that oto.Context doesn't have a Close method.
 func Close() {
 	if player != nil {
-		if done != nil {
-			done <- struct{}{}
-			done = nil
-		}
 		player.Close()
-		context.Close()
 		player = nil
 	}
 }
 
-// Lock locks the speaker. While locked, speaker won't pull new data from the playing Stramers. Lock
+// Lock locks the speaker. While locked, speaker won't pull new data from the playing Streamers. Lock
 // if you want to modify any currently playing Streamers to avoid race conditions.
 //
 // Always lock speaker for as little time as possible, to avoid playback glitches.
@@ -102,16 +87,46 @@ func Clear() {
 	mu.Unlock()
 }
 
-// update pulls new data from the playing Streamers and sends it to the speaker. Blocks until the
-// data is sent and started playing.
-func update() {
-	mu.Lock()
-	mixer.Stream(samples)
-	mu.Unlock()
+// sampleReader is a wrapper for beep.Streamer to implement io.Reader.
+type sampleReader struct {
+	s   beep.Streamer
+	buf [][2]float64
+}
 
-	for i := range samples {
-		for c := range samples[i] {
-			val := samples[i][c]
+func newReaderFromStreamer(s beep.Streamer) *sampleReader {
+	return &sampleReader{
+		s: s,
+	}
+}
+
+// Read pulls samples from the reader and fills buf with the encoded
+// samples. Read expects the size of buf be divisible by the length
+// of a sample (= channel count * bit depth in bytes).
+func (s *sampleReader) Read(buf []byte) (n int, err error) {
+	// Read samples from streamer
+	if len(buf)%bytesPerSample != 0 {
+		return 0, errors.New("requested number of bytes do not align with the samples")
+	}
+	ns := len(buf) / bytesPerSample
+	if len(s.buf) < ns {
+		s.buf = make([][2]float64, ns)
+	}
+	mu.Lock()
+	ns, ok := s.s.Stream(s.buf[:ns])
+	mu.Unlock()
+	if !ok {
+		if s.s.Err() != nil {
+			return 0, errors.Wrap(s.s.Err(), "streamer returned error when requesting samples")
+		}
+		if ns == 0 {
+			return 0, io.EOF
+		}
+	}
+
+	// Convert samples to bytes
+	for i := range s.buf[:ns] {
+		for c := range s.buf[i] {
+			val := s.buf[i][c]
 			if val < -1 {
 				val = -1
 			}
@@ -121,10 +136,10 @@ func update() {
 			valInt16 := int16(val * (1<<15 - 1))
 			low := byte(valInt16)
 			high := byte(valInt16 >> 8)
-			buf[i*4+c*2+0] = low
-			buf[i*4+c*2+1] = high
+			buf[i*bytesPerSample+c*bitDepthInBytes+0] = low
+			buf[i*bytesPerSample+c*bitDepthInBytes+1] = high
 		}
 	}
 
-	player.Write(buf)
+	return ns * bytesPerSample, nil
 }
